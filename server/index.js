321f6f4174dbdbef2758bc617eb12b004063f91d
Miguel Casado <info@mcasado.com>
2022-05-23 22:24:09 +0200

    More changes

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/todos.js";
 
 const app = express();
-
-// app.listen(5000, () => console.log(`Server running on port: ${5000}`));
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
