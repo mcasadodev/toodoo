777867a116e8a9a11734471029e935ee0e6400c5
Miguel Casado <info@mcasado.com>
2022-11-27 23:44:27 +0100

    More changes to make the deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import StaticContext from "context/StaticContext";
 import { UserContextProvider } from "context/UserContext";
@@ -10,13 +9,11 @@ import Wrapper from "components/Wrapper";
 const App = () => {
   return (
     <>
-      <Router>
-        <StaticContext.Provider value={{ key: "value" }}>
-          <UserContextProvider>
-            <Wrapper />
-          </UserContextProvider>
-        </StaticContext.Provider>
-      </Router>
+      <StaticContext.Provider value={{ key: "value" }}>
+        <UserContextProvider>
+          <Wrapper />
+        </UserContextProvider>
+      </StaticContext.Provider>
     </>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import { createRoot } from "react-dom/client";
 
 import App from "App";
@@ -6,4 +8,10 @@ import App from "App";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
