b993932cb824d632fb7bb0d66e517cf1eff25c9e
Miguel Casado <info@mcasado.com>
2022-11-27 10:56:22 +0100

    Tests to deploy

--- index.js
+++ index.js
@@ -25,9 +25,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 if (process.env.ENV === "PRO") {
   app.use(express.static("../client/build"));
   app.get("*", (req, res) => {
-    req.sendFile(
-      path.resolve(__dirname, "..", "client", "build", "index.html")
-    );
+    req.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 } else if (process.env.ENV === "DEV") {
   app.use(
