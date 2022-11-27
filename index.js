ee43b7464607ffe3fa5c46f610baec700acaffad
Miguel Casado <info@mcasado.com>
2022-11-27 21:35:57 +0100

    More changes to make the deployment

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ app.use("/todos", todosRoutes);
 app.use("/participants", participantsRoutes);
 
 if (process.env.ENV === "PRO") {
-  app.use("*", express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.use(express.static("client/build"));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
   app.use(cors());
 } else if (process.env.ENV === "DEV") {
   app.use(
