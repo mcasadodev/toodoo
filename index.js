18e9bbdbe745f7f32ab55a643c34288268d15d8d
Miguel Casado <info@mcasado.com>
2022-12-04 17:55:52 +0100

    Fixes for heroku build

--- index.js
+++ index.js
@@ -24,17 +24,16 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cookieParser());
 
-if (process.env.ENV === "PRO") {
-  app.use(cors());
-} else if (process.env.ENV === "DEV") {
-  app.use(
-    cors({
-      origin: ["http://localhost:3000"],
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: true,
-    })
-  );
-}
+const corsConfig =
+  process.env.ENV === "DEV"
+    ? {
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+      }
+    : null;
+
+app.use(cors(corsConfig));
 
 /* Routes -
  *** MUST GO BELOW CORS SETUP ***
@@ -45,13 +44,13 @@ app.use("/members", membersRoutes);
 app.use("/todos", todosRoutes);
 app.use("/participants", participantsRoutes);
 
-// if (process.env.ENV === "PRO") {
-const { pathname: root } = new URL("./", import.meta.url);
-app.use(express.static("client/build"));
-app.get("*", function (req, res) {
-  console.log(root);
-  res.sendFile(path.join(root, "client/build/index.html"));
-});
-//}
+if (process.env.ENV === "PRO") {
+  const { pathname: root } = new URL("./", import.meta.url);
+  app.use(express.static("client/build"));
+  app.get("*", function (req, res) {
+    console.log(root);
+    res.sendFile(path.join(root, "client/build/index.html"));
+  });
+}
 
 connectDb(app);
